0f7f0a08ae1bb7f5b8c51c94df9f95d02f922fd3
Christina <kristina.ajemyan@gmail.com>
2022-03-04 23:20:46 +0400

    users data

---
+++ data/users.js
@@ -0,0 +1,38 @@
+const users = [
+    {
+        id: 1,
+        name: "John",
+        lastName: "Doe",
+        age: 20
+    },
+    {
+        id: 2,
+        name: "Joe",
+        lastName: "Tribiano",
+        age: 22
+    },
+    {
+        id: 3,
+        name: "Chandler",
+        lastName: "Bing",
+        age: 23
+    },
+    {
+        id: 4,
+        name: "Ross",
+        lastName: "Geller",
+        age: 25
+    },
+    {
+        id: 5,
+        name: "Monica",
+        lastName: "Geller",
+        age: 20
+    },
+    {
+        id: 6,
+        name: "Rachel",
+        lastName: "Grin",
+        age: 20
+    },
+]
