09f5a047302ea4ef23047bccef10db30ee62f2ee
Christina <kristina.ajemyan@gmail.com>
2022-03-05 16:01:58 +0400

    text data

---
+++ data/texts.js
@@ -0,0 +1,11 @@
+const texts = {
+    text1: 'This is John',
+    text2: 'He is a smart boy',
+    text3: 'John is 20 years old',
+    text4: 'this text is not about him',
+    text5: 'this one also',
+    text6: 'John knows it',
+    text7: 'John is really interested in this task',
+    text8: 'You should find everything about him',
+    text9: 'Do no disappoint John'
+}
