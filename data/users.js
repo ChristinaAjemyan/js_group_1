cf1bec1c649fb8af067efb46539aa67360257357
Christina <kristina.ajemyan@gmail.com>
2022-03-12 12:34:05 +0400

    add property

--- data/users.js
+++ data/users.js
@@ -3,36 +3,42 @@ const users = [
         id: 1,
         name: "John",
         lastName: "Doe",
-        age: 20
+        age: 20,
+        isFamous: false
     },
     {
         id: 2,
         name: "Joe",
         lastName: "Tribiano",
-        age: 22
+        age: 22,
+        isFamous: true
     },
     {
         id: 3,
         name: "Chandler",
         lastName: "Bing",
-        age: 23
+        age: 23,
+        isFamous: true
     },
     {
         id: 4,
         name: "Ross",
         lastName: "Geller",
-        age: 25
+        age: 25,
+        isFamous: true
     },
     {
         id: 5,
         name: "Monica",
         lastName: "Geller",
-        age: 20
+        age: 20,
+        isFamous: true
     },
     {
         id: 6,
         name: "Rachel",
         lastName: "Grin",
-        age: 20
+        age: 20,
+        isFamous: true
     },
 ]
