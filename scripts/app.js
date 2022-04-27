b981ea456399f2b8856234673dd7f0eb2d5a4db5
Christina <kristina.ajemyan@gmail.com>
2022-04-27 20:13:18 +0400

    return data

--- scripts/app.js
+++ scripts/app.js
@@ -47,16 +47,92 @@ const countValue = new Promise((resolve, reject) => {
     if (count) {
         resolve("This is resolved")
     } else {
-        reject("This is rejected")
+        // reject("This is rejected")
     }
 })
 
 // console.log(countValue);
 
-countValue.then((result)=>{
-    console.log(result)
-}).catch((res)=>{
-    console.log(res)
-}).finally(()=>{
-    console.log('finally');
-})
+// countValue.then((result)=>{
+//     console.log(result)
+// }).catch((res)=>{
+//     console.log(res)
+// }).finally(()=>{
+//     console.log('finally');
+// })
+
+let object = {
+    x: 1,
+    y: {
+        z: ''
+    }
+}
+
+// try {
+//     console.log(object);
+//     // console.log(x)
+//     // console.log(object.o.p);
+// } catch (err) {
+//     console.log('error', err);
+// } finally {
+//     console.log('finally')
+// }
+let json = '{ "age": 30, "name":"asdsadas" }'; // incomplete data
+
+try {
+
+    let user = JSON.parse(json); // <-- no errors
+
+    if (!user.name) {
+        const err = new SyntaxError("Incomplete data: no name"); // (*)
+        throw err
+    }
+
+    // alert(user.name);
+
+} catch (err) {
+    // alert("JSON Error: " + err.message); // JSON Error: Incomplete data: no name
+}
+
+
+
+
+// throw 10
+
+async function f() {
+
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("done!")
+        }, 1000)
+        console.log(4)
+    });
+
+    let result = await promise; // wait until the promise resolves (*)
+    console.log(3);
+    // alert(result); // "done!"
+}
+
+// console.log(1);
+// f();
+// console.log(2);
+
+
+
+// fetch('https://raw.githubusercontent.com/ChristinaAjemyan/js_group_1/master/data/users-2.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+// console.log('after try catch')
+
+fetch('https://raw.githubusercontent.com/ChristinaAjemyan/js_group_1/master/data/users-2.json',
+    {
+        method: 'GET'
+    })
+    .then(async (response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        console.log(response)
+        const result = await response.json()
+        console.log(result)
+    })
